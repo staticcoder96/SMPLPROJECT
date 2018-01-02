3bb9c85ceb09afca82d28aac49c3f1a2a04ed591
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 23:53:40 -0500

    Update and rename FractalReal.java to SmplReal.java

---
+++ values/SmplReal.java
@@ -0,0 +1,73 @@
+package fractal.values;
+
+import fractal.sys.SmplException;
+import fractal.sys.SmplTypeException;
+
+public class SmplReal extends SmplValue {
+    
+    double value;
+
+    public SmplReal(double d) {
+        super(SmplTypes.REAL);
+        value = d;
+    }
+    
+    @Override
+    public boolean isReal() {
+        return true;
+    }
+    
+    @Override
+    public double realValue() {
+        return value;
+    }
+    
+    public int intValue() {
+        return (int) value;
+    }
+    
+    @Override
+    public SmplValue add(SmplValue val) throws SmplException {
+        if (val.isNumber()) {
+            return new SmplReal(realValue() + val.realValue());
+        } else {
+            throw new SmplTypeException(SmplTypes.REAL, val.getType());
+        }
+    }
+
+    @Override
+    public SmplValue sub(SmplValue val) throws SmplException {
+        if (val.isNumber()) {
+            return new SmplReal(realValue() - val.realValue());
+        } else {
+            throw new SmplTypeException(SmplTypes.REAL, val.getType());
+        }
+    }
+
+    @Override
+    public SmplValue mul(SmplValue val) throws SmplException {
+        if (val.isNumber()) {
+            return new SmplReal(realValue() / val.realValue());
+        } else {
+            throw new SmplTypeException(SmplTypes.REAL, val.getType());
+        }
+    }
+
+    @Override
+    public FractalValue div(FractalValue val) throws FractalException {
+        if (val.isNumber()) {
+            return new FractalReal(realValue() / val.realValue());
+        } else {
+            throw new FractalTypeException(FractalTypes.REAL, val.getType());
+        }
+    }
+    
+    @Override
+    public SmplValue mod(SmplValue val) throws SmplException {
+        if (val.isNumber()) {
+            return new SmplReal(realValue() % val.realValue());
+        } else {
+            throw new SmplTypeException(SmplTypes.REAL, val.getType());
+        }
+    }
+}
