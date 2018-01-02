4896bf8daa1fbcbb0beddb7841ea230a990f2166
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:28:13 -0800

    Update ExpReadInt.java

--- syntax/ExpReadInt.java
+++ syntax/ExpReadInt.java
@@ -1,16 +1,15 @@
-public class ExpReadInt extends Exp {
+public class ExpReadInt extends Exp{
 
   public ExpReadInt() {
-   
   }
 
   @Override
-  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
     return v.visitExpReadInt(this, arg);
   }
 
   @Override
-  public String toString() {
+  public String toString(){
     return "readInt()";
   }
 }
