a58716733204f1f54712943e3d8631afe7e822a4
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:19:11 -0800

    Update ExpRead.java

--- syntax/ExpRead.java
+++ syntax/ExpRead.java
@@ -1,18 +1,8 @@
-package smpl.syntax;
-
-import smpl.semantics.Visitor;
-import smpl.sys.SmplException;
-
-
 public class ExpRead extends Exp {
-
-  
 
   public ExpRead() {
    
   }
-
-  
 
   @Override
   public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
