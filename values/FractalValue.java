99cd32b299ff1c02577e23a61c1af6610f523c7b
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-02 00:01:29 -0500

    Update FractalValue.java

--- values/FractalValue.java
+++ values/FractalValue.java
@@ -1,31 +1,31 @@
-package fractal.values;
+package smpl.values;
 
-import fractal.sys.FractalException;
-import fractal.sys.FractalTypeException;
+import smpl.sys.SmplException;
+import smpl.sys.SmplTypeException;
 
-public class FractalValue {
-    public static final FractalValue NO_VALUE = null;
+public class SmplValue {
+    public static final SmplValue NO_VALUE = null;
     
 //    private Object val;
-    private FractalTypes type;
+    private SmplTypes type;
     
-    public static FractalValue make(int val) {
-        return new FractalInt(val);
+    public static SmplValue make(int val) {
+        return new FSmplInt(val);
     }
     
-    public static FractalValue make(double val) {
-        return new FractalReal(val);
+    public static SmplValue make(double val) {
+        return new SmplReal(val);
     }
     
-    public FractalValue(FractalTypes type) {
+    public SmplValue(Smplypes type) {
         this.type = type;
     }
     
-    protected void setType(FractalTypes t) {
+    protected void setType(SmplTypes t) {
         type = t;
     }
 
-    public FractalTypes getType() {
+    public SmplTypes getType() {
         return type;
     }
     
@@ -34,16 +34,16 @@ public class FractalValue {
      * @return <code>true</code> if this value represents an integer
      */
     public boolean isInt() {
-	return type == FractalTypes.INTEGER;
+	return type == SmplTypes.INTEGER;
     }
     
     /**
      *
      * @return The integer that this value represents
-     * @throws FractalException if this value does not represent an integer 
+     * @throws SmplException if this value does not represent an integer 
      */
-    public int intValue() throws FractalException {
-        throw new FractalTypeException(FractalTypes.INTEGER, type);
+    public int intValue() throws SmplException {
+        throw new SmplTypeException(FSmplTypes.INTEGER, type);
     }
 
     /**
@@ -51,16 +51,16 @@ public class FractalValue {
      * @return <code>true</code> if this value represents a floating point number
      */
     public boolean isReal() {
-	return type == FractalTypes.REAL;
+	return type == SmplTypes.REAL;
     }
     
     /**
      *
      * @return The floating point number that this value represents
-     * @throws FractalException if this value does not represent a real number 
+     * @throws SmplException if this value does not represent a real number 
      */
-    public double realValue() throws FractalException {
-        throw new FractalTypeException(FractalTypes.REAL, type);
+    public double realValue() throws SmplException {
+        throw new SmplTypeException(SmplTypes.REAL, type);
     }
     
     /**
@@ -72,74 +72,60 @@ public class FractalValue {
         return isInt() || isReal();
     }
 
-    /**
-     *
-     * @return <code>true</code> if this value is an instance of a fractal
-     */
-    public boolean isFractal() {
-	return type == FractalTypes.FRACTAL;
-    }
-    
-    public Fractal fractalValue() throws FractalException {
-        if (type == FractalTypes.FRACTAL) 
-            return (Fractal) this;
-        else
-            throw new FractalTypeException(FractalTypes.FRACTAL, type);
-    }
     
     /**
      * Add two values
      * @param val The value to be added to this one.
      * @return The sum of the two values
-     * @throws FractalException if the types of this value and the given one are
+     * @throws SmplException if the types of this value and the given one are
      * incompatible.
      */
-    public FractalValue add(FractalValue val) throws FractalException {
-        throw new FractalTypeException(FractalTypes.REAL, val.getType());
+    public SmplValue add(SmplValue val) throws SmplException {
+        throw new SmplTypeException(SmplTypes.REAL, val.getType());
     }
     
     /**
      * Subtract two values
      * @param val The value to be subtracted from this one.
      * @return The difference of the two values
-     * @throws FractalException if the types of this value and the given one are
+     * @throws SmplException if the types of this value and the given one are
      * incompatible.
      */
-    public FractalValue sub(FractalValue val) throws FractalException {
-        throw new FractalTypeException(FractalTypes.REAL, val.getType());
+    public SmplValue sub(SmplValue val) throws SmplException {
+        throw new SmplTypeException(SmplTypes.REAL, val.getType());
     }
     
     /**
      * Multiply two values
      * @param val The value to be multiplied by this one.
      * @return The product of the two values
-     * @throws FractalException if the types of this value and the given one are
+     * @throws SmplException if the types of this value and the given one are
      * incompatible.
      */
-    public FractalValue mul(FractalValue val) throws FractalException {
-        throw new FractalTypeException(FractalTypes.REAL, val.getType());
+    public SmplValue mul(SmplValue val) throws SmplException {
+        throw new SmplTypeException(SmplTypes.REAL, val.getType());
     }
     
     /**
      * Divide two values to find the quotient
      * @param val The divisor.
      * @return The quotient of this value and the given one.
-     * @throws FractalException if the types of this value and the given one are
+     * @throws SmplException if the types of this value and the given one are
      * incompatible.
      */
-    public FractalValue div(FractalValue val) throws FractalException {
-        throw new FractalTypeException(FractalTypes.REAL, val.getType());
+    public FSmplValue div(SmplValue val) throws SmplException {
+        throw new SmplTypeException(SmplTypes.REAL, val.getType());
     }
     
     /**
      * Divide two values to find the remainder
      * @param val The divisor
      * @return The modulo (remainder) of this value and the given one.
-     * @throws FractalException if the types of this value and the given one are
+     * @throws SmplException if the types of this value and the given one are
      * incompatible.
      */
-    public FractalValue mod(FractalValue val) throws FractalException {
-        throw new FractalTypeException(FractalTypes.REAL, val.getType());
+    public SmplValue mod(SmplValue val) throws SmplException {
+        throw new SmplTypeException(SmplTypes.REAL, val.getType());
     }
     
 }
