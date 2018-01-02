1bcef60bf1bdcc2c070092bf97b3214c63b9ea52
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:14:46 -0800

    Update ExpLogicOr.java

--- syntax/ExpLogicOr.java
+++ syntax/ExpLogicOr.java
@@ -1,13 +1,8 @@
-package MySMPL.syntax;
-
-import MySMPL.semantics.Visitor;
-import MySMPL.sys.SmplException;
-
-public class ExpLogicOr extends Exp {
+public class ExpLogicOr extends Exp{
 
   Exp exp1, exp2;
 
-  public ExpLogicOr(Exp e1, Exp e2) {
+  public ExpLogicOr(Exp e1, Exp e2){
     exp1 = e1;
     exp2 = e2;
   }
@@ -16,17 +11,17 @@ public class ExpLogicOr extends Exp {
     return exp1;
   }
 
-  public Exp getExpR() {
+  public Exp getExpR(){
     return exp2;
   }
 
   @Override
-  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
     return v.visitExpLogicOr(this, arg);
   }
 
   @Override
-  public String toString() {
+  public String toString(){
     return exp1.toString() + " or " + exp2.toString();
   }
 }
