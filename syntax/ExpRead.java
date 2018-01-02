10f79b0e1187c672a61b547d30559a3fc272a5ac
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:33:21 -0800

    Update ExpRead.java

--- syntax/ExpRead.java
+++ syntax/ExpRead.java
@@ -3,14 +3,6 @@ public class ExpRead extends Exp{
   public ExpRead(){
   }
   
-  public ExpRead(Exp exp){
-        this.exp = exp;
-  }
-
-    public Exp getExp(){
-        return exp;
-    }
-
   @Override
   public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
     return v.visitExpRead(this, arg);
@@ -18,6 +10,6 @@ public class ExpRead extends Exp{
 
   @Override
   public String toString(){
-    return "read(" + exp.toString +")";
+    return "read()";
   }
 }
