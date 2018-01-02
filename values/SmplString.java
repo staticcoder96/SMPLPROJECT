2968bdf9dd671fd4ba31e16a2663e4c84e7c8565
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-02 00:27:37 -0500

    Update SmplString.java

--- values/SmplString.java
+++ values/SmplString.java
@@ -1,46 +1,15 @@
 package smpl.values;
 
 import smpl.sys.SmplException;
-import static smpl.values.SmplValue.make;
-import static smpl.values.SmplValue.makeStr;
+import smpl.sys.SmplTypeException;
 
-public class SmplString extends SmplValue<SmplString> {
+public class SmplString extends SmplValue {
 
 	String val;
 
 	public SmplString(String v){
+		super(SmplTypes.STRING;
 		val = v;
-	}
-
-	public static String escape(String str){
-		String newStr = "";
-
-		for (int i=0; i<str.length(); i++){
-			// get current char in string
-			char c = str.charAt(i);
-			// add c to new string unless c is a backslash or the last character in the string
-			if(c != '\\' || i == str.length()) {
-				newStr += c;
-			} else {
-				char nxt = str.charAt(i+1);
-				if(nxt == 'n'){
-					newStr += '\n';
-				} else if(nxt == 't'){
-					newStr += '\t';
-				} else if(nxt == '\\'){
-					newStr += '\\';
-				} else if (nxt == '\"'){
-					newStr += '\"';
-				} else {
-					newStr += c;
-					continue;
-				}
-
-				i++;
-			}
-		}
-
-		return newStr;
 	}
 
 	public SmplType getType(){
@@ -48,60 +17,8 @@ public class SmplString extends SmplValue<SmplString> {
 	}
 
 	@Override
-	public SmplValue<?> add(SmplValue<?> arg) throws SmplException {
-		return makeStr(val + arg.stringValue());
-	}
-
-	public SmplValue<?> eq(SmplValue<?> arg) throws SmplException {
-		if(arg.getType() == SmplType.STRING)
-			return make(val.compareTo(arg.stringValue()) == 0);
-		else
-			return make(false);
-	}
-
-	public SmplValue<?> gt(SmplValue<?> arg) throws SmplException {
-		if(arg.getType() == SmplType.STRING)
-			return make(val.compareTo(arg.stringValue()) > 0);
-		else
-			throw new TypeSmplException(SmplType.STRING, arg.getType());
-	}
-
-	public SmplValue<?> lt(SmplValue<?> arg) throws SmplException {
-		if(arg.getType() == SmplType.STRING)
-			return make(val.compareTo(arg.stringValue()) < 0);
-		else
-			throw new TypeSmplException(SmplType.STRING, arg.getType());
-	}
-
-	public SmplValue<?> le(SmplValue<?> arg) throws SmplException {
-		if(arg.getType() == SmplType.STRING)
-			return make(val.compareTo(arg.stringValue()) <= 0);
-		else
-			throw new TypeSmplException(SmplType.STRING, arg.getType());
-	}
-
-	public SmplValue<?> ge(SmplValue<?> arg) throws SmplException {
-		if(arg.getType() == SmplType.STRING)
-			return make(val.compareTo(arg.stringValue()) >= 0);
-		else
-			throw new TypeSmplException(SmplType.STRING, arg.getType());
-	}
-
-	public SmplValue<?> neq(SmplValue<?> arg) throws SmplException {
-		if(arg.getType() == SmplType.STRING)
-			return make(val.compareTo(arg.stringValue()) != 0);
-		else
-			return make(true);
-	}
-
-	// return literal values
-
-	public int intValue() throws TypeSmplException {
-		return Integer.parseInt(val);
-	}
-
-	public double doubleValue() throws TypeSmplException {
-		return Double.valueOf(val);
+	public SmplValue add(SmplValue arg) throws SmplException {
+		return val + arg.SmplValue();
 	}
 
 	@Override
