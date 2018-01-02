f10a7e34e093dd592dee3b3eeb78a8904b760e44
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:12:26 -0800

    Update ExpLessEq.java

--- syntax/ExpLessEq.java
+++ syntax/ExpLessEq.java
@@ -1,13 +1,8 @@
-package smpl.syntax;
-
-import smpl.semantics.Visitor;
-import smpl.sys.SmplException;
-
-public class ExpLe extends Exp {
+public class ExpLessEq extends Exp{
 
   Exp exp1, exp2;
 
-  public ExpLe(Exp e1, Exp e2) {
+  public ExpLessEq(Exp e1, Exp e2){
     exp1 = e1;
     exp2 = e2;
   }
@@ -16,17 +11,17 @@ public class ExpLe extends Exp {
     return exp1;
   }
 
-  public Exp getExpR() {
+  public Exp getExpR(){
     return exp2;
   }
 
   @Override
-  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
     return v.visitExpLe(this, arg);
   }
 
   @Override
-  public String toString() {
+  public String toString(){
     return exp1.toString() + " <= " + exp2.toString();
   }
 }
