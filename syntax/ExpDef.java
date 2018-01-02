0a470c563d87e4b2713c2c5060867b24c439ae29
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:00:19 -0800

    Update ExpDef.java

--- syntax/ExpDef.java
+++ syntax/ExpDef.java
@@ -1,16 +1,9 @@
-package MySMPL.syntax;
-
-import MySMPL.semantics.Visitor;
-import MySMPL.syntax.Exp;
-import MySMPL.sys.SmplException;
-
-
-public class ExpDef extends Exp {
+public class ExpDef extends Exp{
 
 	String var;
 	Exp exp;
 
-	public ExpDef(String id, Exp e) {
+	public ExpDef(String id, Exp e){
 		var = id;
 		exp = e;
 	}
@@ -24,12 +17,12 @@ public class ExpDef extends Exp {
 	}
 
 	@Override
-	public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+	public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
 		return v.visitExpDef(this, arg);
 	}
 
 	@Override
-	public String toString() {
+	public String toString(){
 		return "def " , var.toString(), exp.toString();
 	}
 }
