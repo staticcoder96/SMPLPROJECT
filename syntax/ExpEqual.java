d47e1b75f6aea649d23e18c146090a31cfec6f4a
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:03:08 -0800

    Update ExpEqual.java

--- syntax/ExpEqual.java
+++ syntax/ExpEqual.java
@@ -1,35 +1,26 @@
-package smpl.syntax;
-
-import smpl.semantics.Visitor;
-import smpl.sys.SmplException;
-
-public class ExpEqual extends Exp {
+public class ExpEqual extends Exp{
 
   Exp exp1, exp2;
 
-  public ExpEqual(Exp e1, Exp e2) {
+  public ExpEqual(Exp e1, Exp e2){
     exp1 = e1;
     exp2 = e2;
-  
   }
 
-  public Exp getExpFirst(){
+  public Exp getExpL(){
     return exp1;
   }
 
-  public Exp getExpSecond(){
+  public Exp getExpR(){
     return exp2;
   }
 
-  
-
   @Override
-  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
     return v.visitExpEqual(this, arg);
   }
 
-  @Override
-  public String toString() {
-    return "equal?(" + exp1.toString() + ", " + exp2.toString() + ")";
+  public String toString(){
+    return "Equal?(" + exp1.toString() + ", " + exp2.toString() + ")";
   }
 }
