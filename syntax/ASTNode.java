d5ec5f602ef3db2b690f8d2efe1424aa913a1cb4
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 16:53:31 -0800

    Update ASTNode.java

--- syntax/ASTNode.java
+++ syntax/ASTNode.java
@@ -1,8 +1,3 @@
-package MySMPL.syntax;
-
-import MySMPL.semantics.Visitor;
-import MySMPL.sys.SmplException;
-
 public abstract class ASTNode {
 
     public abstract <S, T> T visit(Visitor<S, T> v, S state) throws SmplException ;
