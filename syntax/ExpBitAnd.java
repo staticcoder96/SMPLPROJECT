676dcc167a88bf14b3e8919f715763394c1e58e0
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 23:09:48 -0500

    Update ExpBitAnd.java

--- syntax/ExpBitAnd.java
+++ syntax/ExpBitAnd.java
@@ -1,17 +1,22 @@
+package smpl.syntax;
+
+import smpl.semantics.Visitor;
+import smpl.sys.FractalException;
+
 public class ExpBitAnd extends Exp {
 
     Exp exp1, exp2;
 
-    public ExpBitAnd(Exp exp1, Exp exp2) {
+    public ExpBitAnd(Exp exp1, Exp exp2){
         this.exp1= exp1;
         this.exp2 = exp2;
     }
 
-    public Exp getExpL() {
+    public Exp getExpL(){
         return exp1;
     }
 
-    public Exp getExpR() {
+    public Exp getExpR(){
         return exp2;
     }
 
@@ -21,7 +26,7 @@ public class ExpBitAnd extends Exp {
     }
 
     @Override
-    public String toString() {
-        return exp1.toString() + "&" + exp2.toString() ;
+    public String toString(){
+        return exp1.toString() + " & " + exp2.toString() ;
     }
 }
