22d86d67042caf29560804780e4dcce04e0efa04
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:49:17 -0800

    Update makePair.java

--- syntax/makePair.java
+++ syntax/makePair.java
@@ -1,13 +1,8 @@
-package MySMPL.syntax;
-
-import MySMPL.semantics.Visitor;
-import MySMPL.sys.SmplException;
-
-public class ExpPair extends Exp {
+public class ExpPair extends Exp{
 
   Exp exp1, exp2;
 
-  public ExpPair(Exp e1, Exp e2) {
+  public ExpPair(Exp e1, Exp e2){
     exp1 = e1;
     exp2 = e2;
   }
@@ -16,18 +11,18 @@ public class ExpPair extends Exp {
     return exp1;
   }
 
-  public Exp getExpR() {
+  public Exp getExpR(){
     return exp2;
   }
 
   @Override
-  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+  public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
     return v.visitExpPair(this, arg);
   }
 
   @Override
-  public String toString() {
-    return "(" + exp1.toString() + " . " + exp2.toString() + ")";
+  public String toString(){
+    return "(" + exp1.toString() + "." + exp2.toString() + ")";
   }
 }
 
