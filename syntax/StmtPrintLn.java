5e428e6b3ef855cfd5742c7d9b316f3b96a0375f
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:34:15 -0800

    Update StmtPrintLn.java

--- syntax/StmtPrintLn.java
+++ syntax/StmtPrintLn.java
@@ -1,25 +1,25 @@
-public class StmtPrintLn extends Exp {
+public class StmtPrintLn extends Exp{
 
     Exp exp;
 
-    public StmtPrintLn() {
+    public StmtPrintLn(){
     }
 
-    public StmtPrintLn(Exp exp) {
+    public StmtPrintLn(Exp exp){
         this.exp = exp;
     }
 
-    public Exp getExp() {
+    public Exp getExp(){
         return exp;
     }
 
     @Override
-    public <S, T> T visit(SMPLVisitor<S, T> v, S arg) throws SMPLException {
+    public <S, T> T visit(SMPLVisitor<S, T> v, S arg) throws SMPLException{
         return v.visitPrintStmt(this, arg);
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         return "println " + exp.toString();
     }
 }
