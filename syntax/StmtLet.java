32b926d5f5245bcb65a8adc8bcdab2eb285d8744
AnnerChan <33528929+AnnerChan@users.noreply.github.com>
2018-01-01 17:48:09 -0800

    Update StmtLet.java

--- syntax/StmtLet.java
+++ syntax/StmtLet.java
@@ -3,7 +3,7 @@ public class StmtLet extends Exp {
 	ArrayList<Binding> bindings;
 	Exp body;
 
-	public StmtLet(ArrayList<Binding> bindings, Exp body) {
+	public StmtLet(ArrayList<Binding> bindings, Exp body){
 		this.bindings = bindings;
 		this.body = body;
 	}
@@ -17,12 +17,12 @@ public class StmtLet extends Exp {
 	}
 
 	@Override
-	public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException {
+	public <S, T> T visit(Visitor<S, T> v, S arg) throws SmplException{
 		return v.visitStmtLet(this, arg);
 	}
 
 	@Override
 	public String toString() {
-		return "let " + bindings + " in " + body;
+		return "let " + bindings +" "+ body;
 	}
 }
